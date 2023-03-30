85aa441e9a06d375d0bee60fa9285124031e3be5
machinecode-7 <69820905+machinecode-7@users.noreply.github.com>
2023-03-30 19:29:23 +0530

    Session 5

---
+++ 5_Modules/adder.py
@@ -0,0 +1,9 @@
+def sum_nn(number):
+    sum = 0
+    for i in range (1,number+1):
+        sum+=i
+    return sum
+
+
+def sum_nn_2(number):
+    return (number*(number+1))/2
---
+++ 5_Modules/welcome.py
@@ -0,0 +1,5 @@
+def greetings():
+    return "Hello Smart People"
+
+def custom_greetings(name):
+    return "Hello {}, How are you doing ?".format(name)
