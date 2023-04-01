31a7d99bbd54b267143146f1a5cec44aade01974
Himanshu4685 <128665571+Himanshu4685@users.noreply.github.com>
2023-03-31 20:13:17 -0400

    Add files via upload

---
+++ Himanshu/Assignment 5(xyz).py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import xyz
+
+
+# In[2]:
+
+
+ticiket_price = (int(input("please enter your age")))
+xyz.ticiket_price(ticiket_price)
+
+
+# In[ ]:
+
+
+
+
---
+++ Himanshu/xyz.py
@@ -0,0 +1,7 @@
+def ticiket_price(age):
+    if age <=13:
+        return 30
+    elif age >=50:
+        return 30
+    else:
+        return 60
