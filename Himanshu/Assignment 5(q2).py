bbcbe78e06017143a16198dd63c8f495ffd61c40
Himanshu4685 <128665571+Himanshu4685@users.noreply.github.com>
2023-03-31 21:08:46 -0400

    Add files via upload

---
+++ Himanshu/Assignment 5(q2).py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import multi
+
+
+# In[5]:
+
+
+multi.list_multiplication()
+
+
+# In[ ]:
+
+
+
+
---
+++ Himanshu/multi.py
@@ -0,0 +1,9 @@
+def list_multiplication():
+    list_1 = [1,2]
+    list_2 = [3,4]
+    a = []
+    for i in list_1:
+        for j in list_2:
+            a.append(i*j)
+    print(a)
+            
