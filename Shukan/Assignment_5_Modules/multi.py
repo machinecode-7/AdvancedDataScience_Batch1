e9794dbf6cdab772bbda0a48114175789d262dfb
shukan thakkar <94075942+shukanthakkar@users.noreply.github.com>
2023-04-05 21:49:37 +0530

    Add files via upload

---
+++ Shukan/Assignment_5_Modules/multi.py
@@ -0,0 +1,12 @@
+def mul():
+    
+    list1 = [1,2]
+    list2 = [3,4]
+    listx = []
+    
+    for a in list1:
+        for b in list2:
+            
+            listx.append(a*b)
+            
+    print(listx)
---
+++ Shukan/Assignment_5_Modules/ticket.py
@@ -0,0 +1,10 @@
+def ticket(age):
+    
+    if age <= 13:
+        return("your ticket price is only 30$")
+        
+    elif age >=50:
+        return("your ticket price is only 30$")
+        
+    else:
+        return("you have to pay 60$")
