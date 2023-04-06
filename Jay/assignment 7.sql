6288ffaa095e4c335ea8f581bc20ca782f8b679c
Jayiyer9394 <128552908+Jayiyer9394@users.noreply.github.com>
2023-04-05 20:06:53 -0400

    Add files via upload

---
+++ Jay/assignment 7.sql
@@ -0,0 +1,22 @@
+select * from data.province;
+select province from data.province;
+select * from data.province limit 4;
+select * from data.province;
+select distinct province from data.province;
+select * from data.province;
+select * from data.province where province = 'Ontario';
+insert into data.province values ('India', 'IN');
+select * from data.province;
+select distinct province, abbreviation from data.province;
+select distinct province, abbreviation from data.province order by province;
+select distinct province, abbreviation from data.province order by province desc;
+select count(distinct province) from data.province;
+select count(*) from data.province;
+delete from data.province where province = 'india';
+select * from data.province;
+
+
+
+
+
+
