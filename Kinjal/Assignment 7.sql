ccde02362e5880d5227b5bc5074c816dce8370cb
KinjalDR <128445615+KinjalDR@users.noreply.github.com>
2023-04-06 21:30:07 -0400

    Add files via upload

---
+++ Kinjal/Assignment 7.sql
@@ -0,0 +1,19 @@
+select * from kinjal.state;
+select State from kinjal.state;
+select * from kinjal.state limit 5;
+select * from kinjal.state;
+select distinct Abbreviation from kinjal.state;
+select * from kinjal.state;
+select * from kinjal.state where state = 'Colarado';
+insert into kinjal.state values ('Florida', 'FL');
+select * from kinjal.state;
+select distinct state, abbreviation from kinjal.state;
+select distinct state, abbreviation from kinjal.state order by state;
+select distinct state, abbreviation from kinjal.state order by state desc;
+select count(distinct state) from kinjal.state;
+select count(*) from kinjal.state;
+delete from kinjal.state where state = 'Georgia';
+select * from kinjal.state;
+
+
+
