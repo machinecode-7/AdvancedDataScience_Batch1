c4040470f5d9d6b2563f44794101cfa10ba54b66
laksh2808 <128665519+laksh2808@users.noreply.github.com>
2023-04-06 21:34:33 -0400

    Add files via upload

---
+++ Laksh/7_assignment.sql
@@ -0,0 +1,16 @@
+select * from laksh.sample;
+select age_of_woman from laksh.sample;
+select * from laksh.sample limit4;
+select * from laksh.sample;
+select distinct age_of_woman from laksh.sample;
+select * from laksh.sample;
+select * from laksh.sample where age_of_woman = '45 and over';
+insert into laksh.sample values ('2023', '60', '300');
+select * from laksh.sample;
+select distinct age_of_woman from laksh.sample;
+select distinct age_of_woman from laksh.sample order by age_of_woman;
+select distinct age_of_woman from laksh.sample order by age_of_woman desc;
+select count(distinct age_of_woman) from laksh.sample;
+select count(*) from laksh.sample;
+delete from laksh.sample where Period = '2023';
+select * from laksh.sample;
